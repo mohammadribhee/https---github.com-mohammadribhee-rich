38fd1905a4dd27dc9297c89269a2544686138ab2
mohammadribhee <mohammadribhee@gmail.com>
2022-12-12 11:33:35 +0200

    all project rich

---
+++ js/sliddown.js
@@ -0,0 +1,9 @@
+
+  function scrollWin() {
+    window.scrollTo({
+        top: 1000,
+        left: 0,
+        behavior: 'smooth'
+      });
+      
+  }
---
+++ svg/index.tsx
@@ -0,0 +1,7 @@
+export const AllSvg={
+    logo:require('./logo/logo.svg').default,
+    facebook:require('./socialMedia/facebook.svg').default,
+    facebook_hover:require('./socialMedia/facebook_hover.svg').default,
+    instagram:require('./socialMedia/instagram.svg').default,
+    instagram_hover:require('./socialMedia/instagram_hover.svg').default,
+    }
